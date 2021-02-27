a5b905cc66bc409c898a5cb723045a7a705f4224
sshhiinnzzhhiillaa <tulinov.vladislaw@yandex.ru>
2021-02-27 04:02:15 +0300

    Add first model

---
+++ server/src/main/java/com/itr/server/model/Message.java
@@ -0,0 +1,13 @@
+package com.itr.server.model;
+
+import java.io.File;
+import java.util.List;
+
+public class Message {
+    
+    private User addressee;
+    private User sender;
+    private String message;
+    private List<File> files;
+    
+}
